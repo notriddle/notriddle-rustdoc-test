f5c65fa5d15cbc786b7e8da014f6867a0c4d9cad
Michael Howell <michael@notriddle.com>
2021-07-26 08:52:11 -0700

    Add 87484

---
+++ real-headers-stable/implementors/std/error/trait.Error.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["proc_macro"] = [{"text":"impl <a class=\"trait\" href=\"std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"proc_macro/struct.LexError.html\" title=\"struct proc_macro::LexError\">LexError</a>","synthetic":false,"types":["proc_macro::LexError"]}];
+implementors["std"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
